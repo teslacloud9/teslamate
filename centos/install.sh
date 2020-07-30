9cb417615dd6df80b799d4b2f62d3b0f1ad0e7af
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2020-07-30 16:49:42 +0900

    Update install.sh

--- centos/install.sh
+++ centos/install.sh
@@ -2,5 +2,5 @@
 set -e
 # Auto Script For Teslamate
 
-curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/basic/docker-compose.yml" -o ~/docker-compose.yml
+curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/basic/docker-compose.yml" -o docker-compose.yml
 sudo docker-compose up -d
