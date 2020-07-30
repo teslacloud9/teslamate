9b911e071d7f1bb67d8bc08941c6e1c25c9c45db
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2020-07-30 17:09:21 +0900

    Create install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+# Auto Script For Teslamate
+sudo curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/basic/docker-compose.yml" -o docker-compose.yml
+sudo docker-compose up -d
