1d582b88cb697d5178afcc5e60b873624722d28c
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2020-07-30 16:37:40 +0900

    Create install.sh

---
+++ centos/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+# Auto Script For Teslamate
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
