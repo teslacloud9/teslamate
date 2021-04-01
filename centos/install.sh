b75957f7dc74d838cc456c2c45cdbcc40e3a993b
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2021-04-02 00:59:53 +0900

    Update install.sh

--- centos/install.sh
+++ centos/install.sh
@@ -2,6 +2,7 @@
 set -e
 # Auto Script For Teslamate
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.rep
 sudo yum install -y docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
