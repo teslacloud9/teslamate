b3d1b33b607bb34609e31a472d5dc091815732b3
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2021-04-03 01:51:07 +0900

    Update install_auth.sh

--- centos/install_auth.sh
+++ centos/install_auth.sh
@@ -9,6 +9,7 @@ sudo systemctl enable docker
 sudo docker --version
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo rm /usr/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
 curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/auth/docker-compose.yml" -o docker-compose.yml
