5ec31c0f2c85ffb12fa743d7b943d74e3714af9a
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2021-04-02 09:21:08 +0900

    Create install_auth.sh

---
+++ centos/install_auth.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+# Auto Script For Teslamate
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
+sudo yum install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/auth/docker-compose.yml" -o docker-compose.yml
+sudo docker-compose pull
