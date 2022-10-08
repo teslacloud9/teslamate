63473de946c2b7a9cf84f80a9cd4714b1093b042
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2022-10-08 16:30:55 +0900

    Update install.sh

--- oraclecolud/install.sh
+++ oraclecolud/install.sh
@@ -8,7 +8,7 @@ sudo yum install -y docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker --version
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
