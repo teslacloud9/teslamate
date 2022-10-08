7e0ae9324baf9af80fba549d24656638928679b7
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2022-10-08 16:47:58 +0900

    Update install.sh

--- centos/install.sh
+++ centos/install.sh
@@ -6,12 +6,12 @@ set -e
 sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum -y install slirp4netns fuse-overlayfs container-selinux
 
-sudo sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
+#sudo sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
 sudo yum install -y docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker --version
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose --version
