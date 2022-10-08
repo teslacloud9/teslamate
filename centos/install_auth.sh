d9854aeaf6ca25ec6090dae6ba2bb13849c5b245
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2022-10-08 16:49:31 +0900

    Update install_auth.sh

--- centos/install_auth.sh
+++ centos/install_auth.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 set -e
 # Auto Script For Teslamate
-curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/centos/docker-ce.repo" -o docker-ce.repo
-sudo yum-config-manager --add-repo ./docker-ce.repo
+#curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/centos/docker-ce.repo" -o docker-ce.repo
+#sudo yum-config-manager --add-repo ./docker-ce.repo
+sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum -y install slirp4netns fuse-overlayfs container-selinux
 
-sudo sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
+#sudo sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
 sudo yum install -y docker-ce
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker --version
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo rm /usr/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
