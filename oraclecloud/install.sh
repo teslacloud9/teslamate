d6beebb6db42a1bb53d7a772791d3d0c12ec0caa
teslacloud9 <68986787+teslacloud9@users.noreply.github.com>
2022-10-08 16:37:58 +0900

    Create install.sh

---
+++ oraclecloud/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+# Auto Script For Teslamate
+sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum -y install slirp4netns fuse-overlayfs container-selinux
+
+sudo yum install -y docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.11.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+curl -L "https://raw.githubusercontent.com/teslacloud9/teslamate/master/basic/docker-compose.yml" -o docker-compose.yml
+sudo docker-compose up -d
